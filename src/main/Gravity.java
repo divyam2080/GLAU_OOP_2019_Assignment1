fbf6cb0ec27386f9afa2cbcde200c2166d609b08
DIVYAM GUPTA <divyam.gupta_cs18@gla.ac.in>
2019-08-22 20:35:13 +0530

    value scan krke variables le liya aur requirement krdi

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -7,9 +7,19 @@ package main;
 // import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
+import java.util.Scanner;
+
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
+		Scanner d = new Scanner(System.in);
+		double speed;
+		double time;
+		double distance;
+		double gravity = 9.8;
+		distance = 0.5*gravity*time*time;
+		speed = gravity*time
+
 		/* TODO 2:
 		 *   a. Declare all your necessary variables inside main method.
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
